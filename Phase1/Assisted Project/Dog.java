d9830a9dac59ffda62a844b3921f76d13c7ab3d2
Veena Rajesh Badgujar <veenabadgujar23629@gmail.com>
2022-02-08 22:32:25 +0530

    Assignments

---
+++ Phase1/Assisted Project/Dog.java
@@ -0,0 +1,42 @@
+package Phase1;
+
+public class Dog
+{  
+    String name; 
+    String breed; 
+    int age; 
+    String color; 
+    public Dog(String name, String breed, int age, String color) 
+    { 
+        this.name = name; 
+        this.breed = breed; 
+        this.age = age; 
+        this.color = color; 
+    } 
+    public String getName() 
+    { 
+        return name; 
+    } 
+    public String getBreed() 
+    { 
+        return breed; 
+    } 
+    public int getAge() 
+    { 
+        return age; 
+    } 
+    public String getColor() 
+    { 
+        return color; 
+    } 
+    @Override
+    public String toString() 
+    { 
+        return("Hi my name is "+ this.getName()+ ".\nMy breed,age and color are " + this.getBreed()+", " + this.getAge()+ ", and"+ this.getColor() + "."); 
+    } 
+    public static void main(String[] args) 
+    { 
+        Dog scott = new Dog("Tommy","papillon", 6, "black"); 
+        System.out.println(scott.toString()); 
+    } 
+}
