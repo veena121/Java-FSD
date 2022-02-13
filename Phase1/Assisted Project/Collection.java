b0818488d8c864d631af5c531f2eadf8ae6f928e
Veena Rajesh Badgujar <veenabadgujar23629@gmail.com>
2022-02-13 16:10:35 +0530

    Assignments

--- Phase1/Assisted Project/Collection.java
+++ Phase1/Assisted Project/Collection.java
@@ -1,58 +1,44 @@
 package Phase1;
+
 import java.util.*;
 
-public class Collection {
-      public static void main(String[] args) {
+public class Collection{
+
+	public static void main(String[] args) {
 		//creating arraylist
 		System.out.println("ArrayList");
 		ArrayList<String> city=new ArrayList<String>();   
 	      city.add("Jalgaon");//
-	      city.add("Chopda");    	   
+	      city.add("UK");    	   
 	      System.out.println(city);  
-	
 		
-		//creating linkedlist
+		//creating vector
 	      System.out.println("\n");
-	      System.out.println("LinkedList");
-	      LinkedList<String> names=new LinkedList<String>();  
-	      names.add("Swati");  
-	      names.add("Pravin");  	      
-	      Iterator<String> itr=names.iterator();  
-	       while(itr.hasNext()){  
-	       System.out.println(itr.next());  
-	       
-	     //creating vector
-		      System.out.println("\n");
-		      System.out.println("Vector");
-		      Vector<Integer> vec = new Vector();
-		      vec.addElement(100); 
-		      vec.addElement(58); 
-		      System.out.println(vec);
-		      
+	      System.out.println("Vector");
+	      Vector<Integer> vec = new Vector();
+	      vec.addElement(15); 
+	      vec.addElement(30); 
+	      System.out.println(vec);
+		
 	       //creating hashset
 	       System.out.println("\n");
 	       System.out.println("HashSet");
 	       HashSet<Integer> set=new HashSet<Integer>();  
-	       set.add(111);  
-	       set.add(222);  
-	       set.add(333);
-	       set.add(444);
+	       set.add(101);  
+	       set.add(103);  
+	       set.add(102);
+	       set.add(104);
 	       System.out.println(set);
 	       
 	       //creating linkedhashset
 	       System.out.println("\n");
 	       System.out.println("LinkedHashSet");
 	       LinkedHashSet<Integer> set2=new LinkedHashSet<Integer>();  
-	       set2.add(66);  
-	       set2.add(23);  
-	       set2.add(88);
-	       set2.add(32);	       
+	       set2.add(11);  
+	       set2.add(13);  
+	       set2.add(12);
+	       set2.add(14);	       
 	       System.out.println(set2);
 	      	} 
 	      }  
-	}
-
-
-
-
-
+	
